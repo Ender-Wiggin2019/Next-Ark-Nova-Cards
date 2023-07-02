44ae9256b21f13509fee3253489dd4f421f3e7f3
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2023-07-02 22:17:30 +0100

    refactor: move images to /public

---
+++ src/components/icons/actions/Strength.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Strength: React.FC = () => {
+  return (
+    <div className='icon-container icon-container-strength'>
+      <div className='arknova-icon icon-strength'></div>
+    </div>
+  );
+};
+
+export default Strength;
