100c8d04c8d45a9cd54b52b481634334aa8b6bee
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2023-07-05 17:55:03 +0100

    feat: i18n, card filters

---
+++ @types/i18next.d.ts
@@ -0,0 +1,33 @@
+/**
+ * If you want to enable locale keys typechecking and enhance IDE experience.
+ *
+ * Requires `resolveJsonModule:true` in your tsconfig.json.
+ *
+ * @link https://www.i18next.com/overview/typescript
+ */
+import 'i18next';
+
+// resources.ts file is generated with `npm run toc`
+import resources from './resources.ts';
+
+// declare module 'i18next' {
+//   interface CustomTypeOptions {
+//     defaultNS: 'common'
+//     resources: typeof resources
+//   }
+// }
+
+declare module 'react-i18next' {
+  interface CustomTypeOptions {
+    defaultNS: typeof defaultNS;
+    resources: (typeof resources)['en'];
+  }
+}
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    returnNull: false;
+    saveMissing: true;
+    // resources: typeof resources,
+  }
+}
