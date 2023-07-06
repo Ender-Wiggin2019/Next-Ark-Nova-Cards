682511a36d08c8c00eb261865df9741905763f70
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2023-07-06 04:24:46 +0100

    fix: vercel i18n issue

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,6 @@
 // @ts-check
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 /**
  * @type {import('next-i18next').UserConfig}
  */
@@ -11,11 +12,7 @@ module.exports = {
     locales: ['en', 'zh-CN'],
   },
   /** To avoid issues when deploying to some paas (vercel...) */
-  localePath:
-    typeof window === 'undefined'
-      ? // eslint-disable-next-line @typescript-eslint/no-var-requires
-        require('path').resolve('./public/locales')
-      : '/locales',
+  localePath: path.resolve('./public/locales'),
 
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 
