6baa250b163270e55d417af2a41bc09d0628861f
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2023-08-12 06:25:01 +0100

    feat: prisma + planetscale

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -5,8 +5,9 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'development') {
   import('@prisma/client').then((mod) => (prisma = new mod.PrismaClient()));
 } else {
-  import('@prisma/client/edge').then(
-    (mod) => (prisma = new mod.PrismaClient())
-  );
+  // import('@prisma/client/edge').then(
+  //   (mod) => (prisma = new mod.PrismaClient())
+  // );
+  import('@prisma/client').then((mod) => (prisma = new mod.PrismaClient()));
 }
 export { prisma };
