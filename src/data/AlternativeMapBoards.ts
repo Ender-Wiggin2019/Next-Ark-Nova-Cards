4caca6d7a59feb2e6bf2809d56a5e3f21d5750e9
Ender-Wiggin2019 <117010097@link.cuhk.edu.cn>
2024-06-27 11:52:24 +0800

    feat: add new alternative maps fro BGA

---
+++ src/data/AlternativeMapBoards.ts
@@ -0,0 +1,87 @@
+import { MapBoard } from '@/types/MapBoard';
+
+export const AlternativeMapBoards: MapBoard[] = [
+  {
+    id: 'm1a',
+    name: 'maps.name1',
+    image: 'plan1a',
+    description: [
+      'maps.desc1-1',
+      'maps.desc1-2',
+      'maps.desc1-3',
+      'maps.desc1-4',
+    ],
+  },
+  {
+    id: 'm2a',
+    name: 'maps.name2',
+    image: 'plan2a',
+    description: [
+      'maps.desc2-1',
+      'maps.desc2-2',
+      'maps.desc2-3',
+      'maps.desc2-4',
+    ],
+  },
+  {
+    id: 'm3a',
+    name: 'maps.name3',
+    image: 'plan3a',
+    description: ['maps.desc3-1'],
+  },
+  {
+    id: 'm4a',
+    name: 'maps.name4',
+    image: 'plan4a',
+    description: ['maps.desc4-1', 'maps.desc4-2', 'maps.desc4-3'],
+  },
+  {
+    id: 'm5a',
+    name: 'maps.name5',
+    image: 'plan5a',
+    description: ['maps.desc5-1', 'maps.desc5-2', 'maps.desc5-3'],
+  },
+  {
+    id: 'm6a',
+    name: 'maps.name6',
+    image: 'plan6a',
+    description: ['maps.desc6-1', 'maps.desc6-2'],
+  },
+  {
+    id: 'm7a',
+    name: 'maps.name7',
+    image: 'plan7a',
+    description: ['maps.desc7-1'],
+  },
+  {
+    id: 'm8a',
+    name: 'maps.name8',
+    image: 'plan8a',
+    description: ['maps.desc8-1', 'maps.desc8-2', 'maps.desc8-3'],
+  },
+  // {
+  //   id: 'm9',
+  //   name: 'maps.name9',
+  //   image: 'plan9a',
+  //   description: [
+  //     'maps.desc9-1',
+  //     'maps.desc9-2',
+  //     'maps.desc9-3',
+  //     'maps.desc9-4',
+  //     'maps.desc9-5',
+  //     'maps.desc9-6',
+  //   ],
+  // },
+  // {
+  //   id: 'm10',
+  //   name: 'maps.name10',
+  //   image: 'plan10a',
+  //   description: [
+  //     'maps.desc10-1',
+  //     'maps.desc10-2',
+  //     'maps.desc10-3',
+  //     'maps.desc10-4',
+  //     'maps.desc10-5',
+  //   ],
+  // },
+];
--- src/pages/maps/index.tsx
+++ src/pages/maps/index.tsx
@@ -1,11 +1,12 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useTranslation } from 'next-i18next';
+import { Trans, useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { MapBoards } from '@/data/MapBoards';
+import { AlternativeMapBoards } from '@/data/AlternativeMapBoards';
 
 import TextButton from '@/components/buttons/TextButton';
 import { Comments } from '@/components/comments/Comments';
@@ -19,6 +20,10 @@ import {
 } from '@/components/ui/accordion';
 
 import { MapBoard } from '@/types/MapBoard';
+import { Switch } from '@/components/ui/switch';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Sparkles } from 'lucide-react';
+import Link from 'next/link';
 type Props = {
   // Add custom props here
 };
@@ -28,8 +33,10 @@ export default function HomePage(
 ) {
   const router = useRouter();
   const { t } = useTranslation('common');
+  const [alternativeMaps, setAlternativeMaps] = useState(false);
+  const maps = alternativeMaps ? AlternativeMapBoards : MapBoards;
   const queryMapBoard = getMapFromQuery();
-  const [selectedMap, setSelectedMap] = React.useState<MapBoard>(queryMapBoard);
+  const [selectedMap, setSelectedMap] = useState<MapBoard>(queryMapBoard);
   function handleSelectMap(map: MapBoard) {
     setSelectedMap(map);
     router.push('/maps/?map=' + map.id, undefined, { shallow: true });
@@ -37,22 +44,52 @@ export default function HomePage(
 
   useEffect(() => {
     const map = getMapFromQuery();
-    setSelectedMap(map || MapBoards[0]);
-  }, [router.query.map]);
+    setSelectedMap(map || maps[0]);
+  }, [router.query.map, alternativeMaps]);
 
   function getMapFromQuery(): MapBoard {
-    const id = router.query.map ? router.query.map : MapBoards[0].id;
-    const map = MapBoards.find((map) => map.id === id);
-    return map || MapBoards[0];
+    const id = router.query.map ? router.query.map : maps[0].id;
+    const map = maps.find((map) => map.id === id);
+    return map || maps[0];
+  }
+
+  function handleMapChange() {
+    setAlternativeMaps(!alternativeMaps);
   }
   return (
     <Layout>
       <Seo templateTitle='Ark Nova Maps' />
 
-      <div className='flex flex-col px-2 py-2 md:px-4'>
+      <div className='flex flex-col gap-4 px-2 py-2 md:px-4'>
+        <Alert className='bg-lime-500/40'>
+          <Sparkles className='h-4 w-4' />
+          <AlertTitle>{t('maps.alternative_title')}</AlertTitle>
+          <AlertDescription>
+            <div className='flex justify-start gap-4'>
+              <div className='max-w-2xl'>
+                <Trans i18nKey='maps.alternative_desc'>
+                  Check the
+                  <Link
+                    className='font-medium text-primary underline underline-offset-4'
+                    href='https://github.com/Ender-Wiggin2019/Next-Ark-Nova-Cards/tree/main#help-to-translate'
+                  >
+                    post
+                  </Link>
+                  for more information. You can now switch between the original
+                  and alternative maps.
+                </Trans>
+              </div>
+              <Switch
+                checked={alternativeMaps}
+                onCheckedChange={handleMapChange}
+              />
+            </div>
+          </AlertDescription>
+        </Alert>
+
         <div className='grid grid-cols-3 justify-center gap-4 md:grid-cols-3 lg:grid-cols-4'>
-          {MapBoards.map((mapBoard) => (
-            <div key={mapBoard.id} className='w-min justify-self-center'>
+          {maps.map((mapBoard, index) => (
+            <div key={mapBoard.name} className='w-min justify-self-center'>
               <TextButton
                 selected={selectedMap === mapBoard}
                 className='h-12 hover:text-lime-600'
@@ -64,7 +101,7 @@ export default function HomePage(
             </div>
           ))}
         </div>
-        <div className='mt-4 flex w-full flex-col items-start justify-center rounded-lg bg-white/80 p-2 shadow-lg lg:p-4'>
+        <div className='flex w-full flex-col items-start justify-center rounded-lg bg-white/80 p-2 shadow-lg lg:p-4'>
           <Image
             alt={selectedMap.name}
             priority={true}
