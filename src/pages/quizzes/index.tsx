10049c46d3b424bc0afb7d7d713111033df6cfb2
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2024-11-14 00:48:55 +0800

    feat: add discord banner in quiz page

--- src/pages/quizzes/index.tsx
+++ src/pages/quizzes/index.tsx
@@ -1,11 +1,14 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { useTranslation } from 'next-i18next';
+import Link from 'next/link';
+import { Trans, useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import * as React from 'react';
+import { FaDiscord } from 'react-icons/fa';
 
 import Layout from '@/components/layout/Layout';
 import { QuizList } from '@/components/quiz/QuizList';
 import Seo from '@/components/Seo';
+import { Alert, AlertDescription } from '@/components/ui/alert';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function Page(
@@ -18,6 +21,27 @@ export default function Page(
       <Seo />
 
       <main>
+        <div className='mt-4 p-2'>
+          <Alert className='bg-[#5765f2]/90'>
+            <AlertDescription>
+              <div className='flex items-center justify-start gap-4'>
+                <FaDiscord className='h-6 w-6 text-white' />
+                <div className='max-w-2xl text-white'>
+                  <Trans i18nKey='quiz.banner_desc'>
+                    Join the
+                    <Link
+                      className='font-medium text-white underline underline-offset-4'
+                      href='https://discord.gg/8ffMZATB'
+                    >
+                      Discord Group
+                    </Link>
+                    to discuss daily quizzes and any stuff about Ark Nova.
+                  </Trans>
+                </div>
+              </div>
+            </AlertDescription>
+          </Alert>
+        </div>
         <Tabs defaultValue='default' className='mt-6 w-full'>
           <TabsList>
             <TabsTrigger value='default'>
